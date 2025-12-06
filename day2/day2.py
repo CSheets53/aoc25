a22fd59e6fa0c80fa5cc9e4e87bcaf230e435efd
Cooper Sheets <coopersheets@gmail.com>
2025-12-06 17:18:34 -0600

    optimized day 2

--- day2/day2.py
+++ day2/day2.py
@@ -1,3 +1,5 @@
+import time
+
 with open('day2.txt', 'r') as file:
     data = file.readline().split(',')
 
@@ -22,6 +24,7 @@ def part2():
     # add up all invalid ids
     answer = 0
 
+    # my original solution - still ran fast but had a noticeable half-second delay
     for id_range in data:
         start, end = id_range.split('-')[0], id_range.split('-')[1]
         
@@ -39,4 +42,28 @@ def part2():
 
     return answer
 
+t0 = time.time()
 print(f"Part 2: {part2()}")
+t1 = time.time()
+print(f"\tTime: {(t1 - t0):0.3}s")
+
+def part2_new():
+    answer = 0
+
+    # new solution
+    # read that this is similar to LC 459, and there's a trick that if you duplicate a string that repeats, the original will still be in the middle with first and last removed
+    # ex: ABCDABCD -> BCDABCDABCDABC (doubled with first and last removed)
+    # https://seanstuber.com/2025/12/05/advent-of-code-2025-day-2/
+    for id_range in data:
+        start, end = id_range.split('-')[0], id_range.split('-')[1]
+        
+        for id in range(int(start), int(end) + 1):
+            sid = str(id)
+            if sid in (sid + sid)[1:-1]: answer += id
+
+    return answer
+
+t0 = time.time()
+print(f"Part 2 (new): {part2_new()}")
+t1 = time.time()
+print(f"\tTime: {(t1 - t0):0.3}s")
