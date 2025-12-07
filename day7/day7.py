8d53c69aabdef4772309f0978d12a63e869cde41
Cooper Sheets <coopersheets@gmail.com>
2025-12-07 01:54:44 -0600

    day 7 part 2

--- day7/day7.py
+++ day7/day7.py
@@ -24,3 +24,20 @@ def part1():
     return answer
 
 print(f"Part 1: {part1()}")
+
+def part2():
+    first_row = data[0]
+    beam_counts = [0] * len(first_row)
+    beam_counts[first_row.find('S')] = 1
+    for row in data[1:]:
+        if '^' not in row: continue
+
+        for i in range(len(beam_counts)):
+            if beam_counts[i] > 0 and row[i] == '^':
+                beam_counts[i - 1] += beam_counts[i]
+                beam_counts[i + 1] += beam_counts[i]
+                beam_counts[i] = 0
+
+    return sum(beam_counts)
+
+print(f"Part 2: {part2()}")
